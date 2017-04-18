3aef6c5ce878bf0357649d847f68687bf9f36e6f
mathiasody <mathias.ody@cwi.com.br>
2017-04-18 00:59:56 -0300

    feat(Golpe): adicionar classe Golpe

---
+++ M1_Cavaleiros/Golpe.java
@@ -0,0 +1,9 @@
+public class Golpe {
+    String nome;
+    int fatorDano;
+    
+    public Golpe (String nome, int fatorDano) {
+        this.nome = nome;
+        this.fatorDano = fatorDano;
+    }
+}
