963e1960bbf171068f5a405ab96ded194d6e303c
mathiasody <mathias.ody@cwi.com.br>
2017-04-18 15:31:25 -0300

    fix(Constelacao): adicionar equals para comparar dois Golpes

--- M1_Cavaleiros/ConstelacaoTest.java
+++ M1_Cavaleiros/ConstelacaoTest.java
@@ -13,7 +13,7 @@ public class ConstelacaoTest
         dragao.adicionarGolpe(new Golpe ("Cólera do Dragão", 3));
         dragao.adicionarGolpe(new Golpe ("Dragão Voador", 4));
         ArrayList<Golpe> golpes = dragao.getGolpes();
-        assertEquals(golpes.get(0).getNome(), "Cólera do Dragão");
-        assertEquals(golpes.get(1).getNome(), "Dragão Voador");
+        assertEquals(golpes.get(0), new Golpe ("Cólera do Dragão", 3));
+        assertEquals(golpes.get(1), new Golpe ("Dragão Voador", 4));
     }
 }
