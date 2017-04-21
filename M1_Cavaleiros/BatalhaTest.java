44bbc33579602c696dccae91beca6c0122465717
mathiasody <mathias.ody@cwi.com.br>
2017-04-21 13:24:25 -0300

    feat(Movimento): adicionar interface Movimento e cirar o Movimento VestirArmadura.

--- M1_Cavaleiros/BatalhaTest.java
+++ M1_Cavaleiros/BatalhaTest.java
@@ -5,47 +5,55 @@ import org.junit.Test;
 
 public class BatalhaTest
 {
-   @Test
-   public void emSaintsDeMesmaCategoriaOPrimeiroAtaca () throws Exception {
-       SilverSaint saint1 = new SilverSaint ("Silva", "Yolo");
-       SilverSaint saint2 = new SilverSaint ("Santos","Yolo");
-       Batalha batalha = new Batalha (saint1, saint2);
-       batalha.iniciar();
-       assertEquals (90.0, saint2.getVida(),0.001);
-       assertEquals (100.0, saint1.getVida(),0.001);
+    @Test
+    public void emSaintsDeMesmaCategoriaOPrimeiroAtaca () throws Exception {
+        SilverSaint saint1 = new SilverSaint ("Silva", "Yolo");
+        SilverSaint saint2 = new SilverSaint ("Santos","Yolo");
+        Batalha batalha = new Batalha (saint1, saint2);
+        batalha.iniciar();
+        assertEquals (90.0, saint2.getVida(),0.001);
+        assertEquals (100.0, saint1.getVida(),0.001);
     }
-    
+
     @Test
     public void primeiroSaintCategoriaMaisAltaSegundoTomaDano () throws Exception {
-       GoldSaint saint1 = new GoldSaint ("Máscara Da Morte","Câncer");
-       SilverSaint saint2 = new SilverSaint ("Santos", "Yolo");
-       Batalha batalha = new Batalha (saint1, saint2);
-       batalha.iniciar();
-       assertEquals (90.0, saint2.getVida(),0.001);
-       assertEquals (100.0, saint1.getVida(),0.001);
-       
-       SilverSaint saint3 = new SilverSaint ("Silva","Yolo");
-       BronzeSaint saint4 = new BronzeSaint ("Santos","Yolo");
-       Batalha batalha2 = new Batalha (saint3, saint4);
-       batalha2.iniciar();
-       assertEquals (100.0, saint3.getVida(),0.001);
-       assertEquals (90.0, saint4.getVida(),0.001);
+        GoldSaint saint1 = new GoldSaint ("Máscara Da Morte","Câncer");
+        SilverSaint saint2 = new SilverSaint ("Santos", "Yolo");
+        Batalha batalha = new Batalha (saint1, saint2);
+        batalha.iniciar();
+        assertEquals (90.0, saint2.getVida(),0.001);
+        assertEquals (100.0, saint1.getVida(),0.001);
+
+        SilverSaint saint3 = new SilverSaint ("Silva","Yolo");
+        BronzeSaint saint4 = new BronzeSaint ("Santos","Yolo");
+        Batalha batalha2 = new Batalha (saint3, saint4);
+        batalha2.iniciar();
+        assertEquals (100.0, saint3.getVida(),0.001);
+        assertEquals (90.0, saint4.getVida(),0.001);
     }
-    
+
     @Test
     public void segundoSaintCategoriaMaisAltaPrimeiroTomaDano () throws Exception {
-       SilverSaint saint1 = new SilverSaint ("Silva", "Yolo");
-       GoldSaint saint2 = new GoldSaint ("Máscara Da Morte", "Câncer");
-       Batalha batalha = new Batalha (saint1, saint2);
-       batalha.iniciar();
-       assertEquals (90.0, saint1.getVida(),0.001);
-       assertEquals (100.0, saint2.getVida(),0.001);
-       
-       BronzeSaint saint3 = new BronzeSaint ("Silva", "Yolo");
-       SilverSaint saint4 = new SilverSaint ("Santos", "Yolo");
-       Batalha batalha2 = new Batalha (saint3, saint4);
-       batalha2.iniciar();
-       assertEquals (100.0, saint4.getVida(),0.001);
-       assertEquals (90.0, saint3.getVida(),0.001);
+        SilverSaint saint1 = new SilverSaint ("Silva", "Yolo");
+        GoldSaint saint2 = new GoldSaint ("Máscara Da Morte", "Câncer");
+        Batalha batalha = new Batalha (saint1, saint2);
+        batalha.iniciar();
+        assertEquals (90.0, saint1.getVida(),0.001);
+        assertEquals (100.0, saint2.getVida(),0.001);
+
+        BronzeSaint saint3 = new BronzeSaint ("Silva", "Yolo");
+        SilverSaint saint4 = new SilverSaint ("Santos", "Yolo");
+        Batalha batalha2 = new Batalha (saint3, saint4);
+        batalha2.iniciar();
+        assertEquals (100.0, saint4.getVida(),0.001);
+        assertEquals (90.0, saint3.getVida(),0.001);
+    }
+
+    @Test
+    public void vestirArmaduraComMovimentoVestirArmadura () throws Exception {SilverSaint saint1 = new SilverSaint ("Silva", "Yolo");
+        GoldSaint saint = new GoldSaint ("Máscara Da Morte", "Câncer");
+        VestirArmadura vestir = new VestirArmadura(saint);
+        vestir.executar();
+        assertEquals (true, saint.getArmaduraVestida());
     }
 }
