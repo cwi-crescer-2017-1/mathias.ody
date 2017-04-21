7a5e950c366a21bb09a0de15d45a9b7b726c45f8
mathiasody <mathias.ody@cwi.com.br>
2017-04-21 14:44:08 -0300

    feat(Batalha): adicionar classe Golpear e implementar seu funcionamento.

--- M1_Cavaleiros/BatalhaTest.java
+++ M1_Cavaleiros/BatalhaTest.java
@@ -50,10 +50,80 @@ public class BatalhaTest
     }
 
     @Test
-    public void vestirArmaduraComMovimentoVestirArmadura () throws Exception {SilverSaint saint1 = new SilverSaint ("Silva", "Yolo");
+    public void vestirArmaduraComMovimentoVestirArmadura () throws Exception {
         GoldSaint saint = new GoldSaint ("Máscara Da Morte", "Câncer");
         VestirArmadura vestir = new VestirArmadura(saint);
         vestir.executar();
         assertEquals (true, saint.getArmaduraVestida());
+    }
+    
+    @Test
+    public void golpearSemArmadura () throws Exception {
+        SilverSaint prata = new SilverSaint ("Jamian", "Corvo");
+        GoldSaint ouro = new GoldSaint ("Máscara Da Morte", "Câncer");
+        Golpear golpear = new Golpear(ouro, prata);
+        ouro.aprenderGolpe(new Golpe ("Ondas do Inferno", 10));
+        golpear.executar();
+        assertEquals (90.0, prata.getVida(), 0.01);
+    }
+    
+    @Test
+    public void golpearSemArmaduraListaGolpes () throws Exception {
+        SilverSaint prata = new SilverSaint ("Jamian", "Corvo");
+        GoldSaint ouro = new GoldSaint ("Saga", "Gêmeos");
+        Golpear ouroGolpear = new Golpear(ouro, prata);
+        Golpear prataGolpear = new Golpear(prata, ouro);
+        ouro.aprenderGolpe(new Golpe ("Outra Dimensão", 5));
+        ouro.aprenderGolpe(new Golpe ("Explosão Galática", 10));
+        prata.aprenderGolpe (new Golpe ("Eixo das Asas Negras", 5));
+        prata.aprenderGolpe (new Golpe ("Bicada Louca do Corvo", 3));
+        ouroGolpear.executar();
+        prataGolpear.executar();
+        ouroGolpear.executar();
+        prataGolpear.executar();
+        ouroGolpear.executar();
+        prataGolpear.executar();
+        assertEquals (80.0, prata.getVida(), 0.01);
+        assertEquals (87.0, ouro.getVida(), 0.01);
+    }
+    
+    @Test
+    public void golpearComArmadura () throws Exception {
+        SilverSaint prata = new SilverSaint ("Jamian", "Corvo");
+        GoldSaint ouro = new GoldSaint ("Máscara Da Morte", "Câncer");
+        VestirArmadura vestirOuro = new VestirArmadura(ouro);
+        vestirOuro.executar();
+        VestirArmadura vestirPrata = new VestirArmadura(prata);
+        vestirPrata.executar();
+        
+        Golpear golpear = new Golpear(ouro, prata);
+        ouro.aprenderGolpe(new Golpe ("Ondas do Inferno", 10));
+        golpear.executar();
+        assertEquals (60.0, prata.getVida(), 0.01);
+    }
+    
+    @Test
+    public void golpearComArmaduraListaGolpes () throws Exception {
+        SilverSaint prata = new SilverSaint ("Jamian", "Corvo");
+        GoldSaint ouro = new GoldSaint ("Saga", "Gêmeos");
+        VestirArmadura vestirOuro = new VestirArmadura(ouro);
+        vestirOuro.executar();
+        VestirArmadura vestirPrata = new VestirArmadura(prata);
+        vestirPrata.executar();
+        
+        Golpear ouroGolpear = new Golpear(ouro, prata);
+        Golpear prataGolpear = new Golpear(prata, ouro);
+        ouro.aprenderGolpe(new Golpe ("Outra Dimensão", 5));
+        ouro.aprenderGolpe(new Golpe ("Explosão Galática", 10));
+        prata.aprenderGolpe (new Golpe ("Eixo das Asas Negras", 5));
+        prata.aprenderGolpe (new Golpe ("Bicada Louca do Corvo", 3));
+        ouroGolpear.executar();
+        prataGolpear.executar();
+        ouroGolpear.executar();
+        prataGolpear.executar();
+        ouroGolpear.executar();
+        prataGolpear.executar();
+        assertEquals (20.0, prata.getVida(), 0.01);
+        assertEquals (61.0, ouro.getVida(), 0.01);
     }
 }
---
+++ M1_Cavaleiros/Golpear.java
@@ -0,0 +1,20 @@
+public class Golpear implements Movimento
+{
+    private Saint golpeador;
+    private Saint golpeado;
+    public Golpear (Saint golpeador, Saint golpeado) {
+        this.golpeador = golpeador;
+        this.golpeado = golpeado;
+    }
+    
+    public void executar () {
+        int danoBase = golpeador.getProximoGolpe().getFatorDano();
+        if (golpeador.getArmaduraVestida()){
+            int multiplicador = golpeador.getValorCategoriaArmadura() + 1;
+            golpeado.perderVida((double)danoBase * multiplicador);
+        }
+        else {
+            golpeado.perderVida((double)danoBase);
+        }
+    }
+}
