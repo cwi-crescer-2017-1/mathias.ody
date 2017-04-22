90ada35bd97daa01ebca2953f2f9cff945569980
mathiasody <mathias.ody@cwi.com.br>
2017-04-22 01:36:15 -0300

    refactor(GoldSaint): otimizar a verificação referente a se a constelação passada faz parte da lista de signos.

--- M1_Cavaleiros/GoldSaint.java
+++ M1_Cavaleiros/GoldSaint.java
@@ -2,20 +2,19 @@ public class GoldSaint extends Saint {
     public GoldSaint (String nome, String nomeConstelacao) throws Exception {
         super (nome, new Armadura (new Constelacao (nomeConstelacao), Categoria.OURO));
         this.qtdeSentidosDespertados = 7;
-        
-        String constelacao = armadura.getConstelacao().getNome();
-            if ( !constelacao.equals("Áries") 
-            && !constelacao.equals("Touro")
-            && !constelacao.equals("Gêmeos")
-            && !constelacao.equals("Câncer")
-            && !constelacao.equals("Virgem")
-            && !constelacao.equals("Leão")
-            && !constelacao.equals("Libra")
-            && !constelacao.equals("Escorpião")
-            && !constelacao.equals("Sagitário")
-            && !constelacao.equals("Capricórnio")
-            && !constelacao.equals("Aquário")
-            && !constelacao.equals("Peixes")) {
+
+            if ( !nomeConstelacao.equals("Áries") 
+            && !nomeConstelacao.equals("Touro")
+            && !nomeConstelacao.equals("Gêmeos")
+            && !nomeConstelacao.equals("Câncer")
+            && !nomeConstelacao.equals("Virgem")
+            && !nomeConstelacao.equals("Leão")
+            && !nomeConstelacao.equals("Libra")
+            && !nomeConstelacao.equals("Escorpião")
+            && !nomeConstelacao.equals("Sagitário")
+            && !nomeConstelacao.equals("Capricórnio")
+            && !nomeConstelacao.equals("Aquário")
+            && !nomeConstelacao.equals("Peixes")) {
                 // dar erro
                 throw new Exception("Constelação inválida");
             }
