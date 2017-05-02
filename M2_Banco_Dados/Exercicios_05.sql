1b45af6b1548002a7d7614c72c2b5eb2ec6bdf95
mathiasody <mathias.ody@cwi.com.br>
2017-05-02 16:53:38 -0300

    feat(Exercicio5).

---
+++ M2_Banco_Dados/Exercicios_05.sql
@@ -0,0 +1,45 @@
+/*
+1 - Liste o nome do empregado, o nome do gerente e o departamento de cada um.
+*/
+
+SELECT * FROM Empregado; 
+
+SELECT e.NomeEmpregado,
+	   g.NomeEmpregado as NomeGerente,
+	   d.NomeDepartamento
+	   FROM Empregado e
+	   INNER JOIN Departamento d ON e.IDDepartamento = d.IDDepartamento
+	   INNER JOIN Empregado g ON g.IDEmpregado = e.IDGerente;
+
+/*
+2 - Liste o deparamento (id e nome) com o empregado de maior salário.
+*/
+
+SELECT DISTINCT
+	d.IDDepartamento,
+	d.NomeDepartamento
+	FROM  Empregado e
+	INNER JOIN Departamento d ON e.IDDepartamento = d.IDDepartamento
+	WHERE e.Salario IN (SELECT MAX(Salario)
+						 FROM Empregado
+						 WHERE (IDDepartamento IS NOT NULL))
+
+/*
+3 - Aplique uma alteração salarial em todos os empregados que o departamento fique na localidade de SAO PAULO, este reajuste 
+deve ser de 17,3%. Por segurança faça uma cópia da tabela Empregado antes de iniciar esta tarefa.
+*/
+
+
+BEGIN TRANSACTION
+UPDATE Empregado
+	SET Salario = e.Salario + (Salario * 0.173)
+	FROM Empregado e
+	INNER JOIN Departamento d ON d.IDDepartamento = e.IDDepartamento
+	WHERE d.Localizacao = 'SAO PAULO' 
+
+COMMIT
+
+/*
+4 - Liste todas as cidades duplicadas (nome e UF iguais).
+*/
+
