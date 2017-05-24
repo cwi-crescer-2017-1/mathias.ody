ea043f342d48a3c6247134a4a5ca33bb0c6cc79c
mathiasody <mathias.ody@cwi.com.br>
2017-05-24 17:29:01 -0300

    feat(cSharp): aula 01.

---
+++ M6_dotNET/Aula1/Demo1/CalculoIMC.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    public class CalculoIMC
+    {
+        public double Altura { get; set; }
+        public int Peso { get; set; }
+        public CalculoIMC (double altura, int peso)
+        {
+            Altura = altura;
+            Peso = peso;
+        }
+
+        public double CalcularIMC()
+        {
+            return Peso / (Math.Pow(Altura,2));
+        }
+    }
+}
---
+++ M6_dotNET/Aula1/Demo1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*var entradas = new int[] { };
+
+            while (true)
+            {
+                Console.WriteLine("Digite um valor:");
+                var entrada = Console.ReadLine();
+
+                if (entrada == "exit")
+                {
+                    break;
+                }
+                Array.Resize(ref entradas, entradas.Length + 1);
+                entradas[entradas.Length - 1] = int.Parse(entrada);
+            }
+
+            string resultado = "";
+            foreach (int i in entradas)
+            {
+                resultado += i + " ";
+            }
+            Console.WriteLine(resultado);
+            Console.ReadKey();*/
+
+
+            List<int> entradas = new List<int>();
+
+            while (true)
+            {
+                Console.WriteLine("Digite um valor:");
+                var entrada = Console.ReadLine();
+
+                if (entrada == "exit")
+                {
+                    break;
+                }
+                entradas.Add(int.Parse(entrada));
+            }
+
+            string resultado = "";
+            foreach (int i in entradas)
+            {
+                resultado += i + " ";
+            }
+            Console.WriteLine(resultado);
+            Console.ReadKey();
+        }
+    }
+}
