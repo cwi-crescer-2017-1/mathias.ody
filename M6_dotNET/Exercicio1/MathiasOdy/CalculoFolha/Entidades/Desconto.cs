17f45068d58af24416e0e64959004b21d8f65e37
Unknown <mathias.ody@cwi.com.br>
2017-05-25 03:34:15 -0300

    refactor(Desconto): alterar a forma como o Desconto é calculado.

---
+++ M6_dotNET/Exercicio1/MathiasOdy/CalculoFolha/Entidades/Desconto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CalculoFolha.Entidades
+{
+    public class Desconto
+    {
+        public Desconto(double aliquota, double salario)
+        {
+            Aliquota = aliquota;
+            Valor = Utils.WeirdRound (salario * aliquota,2);
+        }
+
+        public double Aliquota { get; private set; }
+        public double Valor { get; private set; }
+    }
+}
