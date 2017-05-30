f2b0a10643bbcc3cb8e728030911e22d1934fa9a
mathiasody <mathias.ody@cwi.com.br>
2017-05-30 17:38:36 -0300

    feat(dotnet): aula sei lá o numero.

---
+++ M6_dotNET/Aula5/EditoraCrescer/EditoraCrescer.Api/Controllers/LivrosController.cs
@@ -0,0 +1,21 @@
+﻿using EditoraCrescer.Infraestrutura;
+using EditoraCrescer.Infraestrutura.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EditoraCrescer.Api.Controllers
+{
+    public class LivrosController : ApiController
+    {
+        private LivroRepositorio repositorio = new LivroRepositorio();
+
+        public IHttpActionResult Get()
+        {
+            return Ok(repositorio.Obter());
+        }
+    }
+}
