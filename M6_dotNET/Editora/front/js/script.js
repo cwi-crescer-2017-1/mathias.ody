53d9826289d7513693e57f83f2f6be7a88506cdb
mathiasody <mathiasluizody@hotmail.com>
2017-06-02 12:22:21 -0300

    feat(editora): melhorar design da página.

--- M6_dotNET/Editora/front/js/script.js
+++ M6_dotNET/Editora/front/js/script.js
@@ -11,11 +11,13 @@ app.config (function ($routeProvider) {
         .otherwise({redirectTo: '/'});
 });
 
-app.controller('livroController', function ($scope,
+app.controller('livroController', function ($window,
+                                            $scope,
                                             $routeParams,
                                             livroService){
 
     listarLancamentos();
+    setTimeout(resize, 500)
 
     function listarLancamentos() {
         livroService.lancamentos().then(function(response){
@@ -23,4 +25,31 @@ app.controller('livroController', function ($scope,
             console.log(response.data.dados);
         })
     }
+    addEvent(window, "resize", resizeEvent);
 })
+
+function resizeEvent (event) {
+    resize();
+}
+
+function resize () {
+    let list = document.getElementsByClassName("livros");
+    let width = 0;
+    for (let i = 0; i < list.length; i++) {
+        if (i == 0) {
+            width = list[i].clientWidth;
+        }
+        list[i].style.height = '' + width *2 + 'px';
+    }
+}
+
+var addEvent = function(object, type, callback) {
+    if (object == null || typeof(object) == 'undefined') return;
+    if (object.addEventListener) {
+        object.addEventListener(type, callback, false);
+    } else if (object.attachEvent) {
+        object.attachEvent("on" + type, callback);
+    } else {
+        object["on"+type] = callback;
+    }
+};
