b3e5dc9ad204b508556352be29b0d0e80ef41e78
mathiasody <mathias.ody@cwi.com.br>
2017-06-27 17:30:06 -0300

    feat(java): aula 6.

---
+++ M8_Java/aula6/src/main/java/Hello.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mathias.ody
+ */
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class Hello {
+
+    private String helloWorld = "Hello World!";
+
+    public String getHelloWorld() {
+        return helloWorld;
+    }
+
+    public void setHelloWorld(String helloWorld) {
+        this.helloWorld = helloWorld;
+    }
+
+}
